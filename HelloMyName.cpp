835de67ef9fc18512c2c2ba6ca6cdb24d41d62cf
DeeLite <deelite@deelitestudios.com>
2024-06-20 17:07:27 -0400

    Our first commit

---
+++ HelloMyName.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string myName;
+    cout << "Please enter your name : ";
+    cin >> myName;
+    cout << "Hello World, my name is " << myName << "!" << endl;
+    return 0;
+}
